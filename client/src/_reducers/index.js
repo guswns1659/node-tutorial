4c74b1d3dbc676186ae4f2cc64d9ca1db963ed6a
Jack <jack.comeback@kakaopaycorp.com>
2021-09-20 09:55:47 +0900

    feat: Add LoginPage

--- client/src/_reducers/index.js
+++ client/src/_reducers/index.js
@@ -1,11 +1,11 @@
 import {combineReducers} from "redux";
-// import user from './user_reducer'
+import user from './user_reducer'
 
 /**
  * 여러 reducer를 합쳐주는 기능. state 종류에 따라 reducer를 나눌 수 있다. ex. user, comments
  */
 const rootReducer = combineReducers({
-    // user,
+    user
 })
 
 export default rootReducer;
--- client/src/components/views/LogingPage/LoginPage.js
+++ client/src/components/views/LogingPage/LoginPage.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useDispatch} from "react-redux";
+import {loginUser} from '../../../_actions/user_action';
 
 function LoginPage(props) {
+    const disPatch = useDispatch();
+
+    /** Init state instead of props, because state is used in one component.
+     */
+    const [Email, setEmail] = useState("");
+    const [Password, setPassword] = useState("");
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSumbitHandler = (event) => {
+        /** prevent page refreshing.
+         */
+        event.preventDefault();
+
+        let body = {
+            email: Email,
+            password: Password
+        }
+
+        disPatch(loginUser(body))
+            .then(response => {
+                if(response.payload.loginSuccess) {
+                    props.history.push('/')
+                } else {
+                    alert('Error')
+                }
+            })
+    }
+
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}>
+            <form style={{display: 'flex', flexDirection: 'column'}}
+                onSubmit={onSumbitHandler}
+            >
+
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler}/>
+                <label>Password</label>
+                <input type="Password" value={Password} onChange={onPasswordHandler}/>
+                <br/>
+                <button>
+                    Login
+                </button>
+            </form>
         </div>
     );
 }

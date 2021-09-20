ba699b71a37dbe799d370b5f8dc420ecda883dd9
Jack <jack.comeback@kakaopaycorp.com>
2021-09-20 09:55:47 +0900

    feat: Add LoginPage

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -4,14 +4,18 @@ import axios from "axios";
 
 function LandingPage() {
 
-    // CORS test using proxy
     useEffect(() => {
         axios.get('/api/hello').then(r => console.log(r))
     })
 
     return (
-        <div>
-            LandingPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}>
+            <h2>
+                시작페이지
+            </h2>
         </div>
     );
 }
--- client/src/setUpProxy.js
+++ client/src/setUpProxy.js
@@ -1,6 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
+    // CORS test using proxy
     app.use(
         '/api',
         createProxyMiddleware({
